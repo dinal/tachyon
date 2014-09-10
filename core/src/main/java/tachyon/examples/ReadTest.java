d8117faae60090062f628c239885e7f704f0f1a3
Dina Leventol <dinal@mellanox.com>
2014-09-10 11:09:53 +0300

    add to benchmark support for reading several files

--- core/src/main/java/tachyon/examples/ReadTest.java
+++ core/src/main/java/tachyon/examples/ReadTest.java
@@ -23,6 +23,7 @@ public class ReadTest {
   private static String operation = null;
   private static long bytes = 0;
   private static boolean checkData = false;
+  private static int numFiles = 0;
 
   public static void main(String[] args) throws IOException {
     if (args.length < 4) {
@@ -30,15 +31,16 @@ public class ReadTest {
           .println("java -cp target/tachyon-"
               + Version.VERSION
               + "-jar-with-dependencies.jar "
-              + "tachyon.examples.ReadTest <TachyonMasterAddress> <FilePath> <Operation> <FileBytes> <optional_checkData>");
+              + "tachyon.examples.ReadTest <TachyonMasterAddress> <FilePath> <Operation> <FileBytes> <numFilesRead> <optional_checkData>");
       System.exit(-1);
     }
     sTachyonClient = TachyonFS.get(args[0]);
     sFilePath = args[1];
     operation = args[2];
     bytes = Long.parseLong(args[3]);
-    if (args.length == 5) {
-      checkData = Boolean.parseBoolean(args[4]);
+    numFiles = Integer.parseInt(args[4]);
+    if (args.length == 6) {
+      checkData = Boolean.parseBoolean(args[5]);
     }
 
     if (operation.compareTo("read") == 0) {
@@ -69,31 +71,37 @@ public class ReadTest {
   }
 
   public static void readFile() throws IOException {
-    long time = 0;
-    int read = 0;
-    TachyonFile file = sTachyonClient.getFile(sFilePath);
-    System.out.println("Going to read file " + sFilePath + " size " + file.length());
-    time = System.nanoTime();
-    InStream is = file.getInStream(ReadType.NO_CACHE);
-    if (checkData) {
-      LOG.info("validating data");
-      long k = 0;
-      time = System.nanoTime();
-      while (read != -1) {
-        read = is.read();
-        if (read != -1 && (byte) read != (byte) k) {
-          LOG.error("ERROR IN TEST, got " + (byte) read + " expected " + (byte) k);
-          return;
+    try {
+      for (int i = 1; i <= numFiles; i ++) {
+        long time = 0;
+        int read = 0;
+        TachyonFile file = sTachyonClient.getFile(sFilePath + "." + i);
+        LOG.info("Going to read file " + sFilePath + "." + i + " size " + file.length());
+        time = System.nanoTime();
+        InStream is = file.getInStream(ReadType.NO_CACHE);
+        if (checkData) {
+          LOG.info("validating data");
+          long k = 0;
+          while (read != -1) {
+            read = is.read();
+            if (read != -1 && (byte) read != (byte) k) {
+              LOG.error("ERROR IN TEST, got " + (byte) read + " expected " + (byte) k);
+              return;
+            }
+            k ++;
+          }
+        } else {
+          byte[] buf = new byte[4 * 1024];
+          while (read != -1) {
+            read = is.read(buf);
+          }
         }
-        k ++;
+        time = System.nanoTime() - time;
+        System.out.println("Finished reading file " + sFilePath + "." + i + " time " + time + " \n");
+        Thread.currentThread().sleep(10000);
       }
-    } else {
-      byte[] buf = new byte[4 * 1024];
-      while (read != -1) {
-        read = is.read(buf);
-      }
+    } catch (Exception e) {
+      LOG.error("got Exception is test "+e.toString());
     }
-    time = System.nanoTime() - time;
-    System.out.println("Finished reading file " + sFilePath + " time " + time+" ");
   }
 }
