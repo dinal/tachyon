99cb7dbc7b6e25f557f0f7c471c3bcf1008dc06c
Dina Leventol <dinal@mellanox.com>
2014-09-11 11:56:50 +0300

    change ReadTest to recieve file list for reading/writing

--- core/src/main/java/tachyon/examples/ReadTest.java
+++ core/src/main/java/tachyon/examples/ReadTest.java
@@ -19,11 +19,10 @@ public class ReadTest {
 
   private static Logger LOG = Logger.getLogger(Constants.LOGGER_TYPE);
   private static TachyonFS sTachyonClient;
-  private static String sFilePath = null;
   private static String operation = null;
   private static long bytes = 0;
   private static boolean checkData = false;
-  private static int numFiles = 0;
+  private static String[] files = null;
 
   public static void main(String[] args) throws IOException {
     if (args.length < 4) {
@@ -31,52 +30,57 @@ public class ReadTest {
           .println("java -cp target/tachyon-"
               + Version.VERSION
               + "-jar-with-dependencies.jar "
-              + "tachyon.examples.ReadTest <TachyonMasterAddress> <FilePath> <Operation> <FileBytes> <numFilesRead> <optional_checkData>");
+              + "tachyon.examples.ReadTest <TachyonMasterAddress> <Operation> <FileList_CommaSeparated> <FileBytes> <Optional_CheckData>");
       System.exit(-1);
     }
     sTachyonClient = TachyonFS.get(args[0]);
-    sFilePath = args[1];
-    operation = args[2];
+    operation = args[1];
+    files = args[2].split(",");
     bytes = Long.parseLong(args[3]);
-    numFiles = Integer.parseInt(args[4]);
-    if (args.length == 6) {
-      checkData = Boolean.parseBoolean(args[5]);
+    if (args.length == 5) {
+      checkData = Boolean.parseBoolean(args[4]);
     }
 
-    if (operation.compareTo("read") == 0) {
-      readFile();
+    LOG.info("Going to " + operation + " " + files.length + " files with " + bytes + " bytes, "
+        + args[2]);
+    if (operation.equals("read")) {
+      readFiles();
     } else {
-      createFile(sFilePath);
-      writeFile();
+      createFiles();
+      writeFiles();
     }
 
     System.exit(0);
   }
 
-  public static void createFile(String path) throws IOException {
-    int fileId = sTachyonClient.createFile(path);
-    System.out.println("File " + path + " created with id " + fileId);
-  }
-
-  public static void writeFile() throws IOException {
-    TachyonFile file = sTachyonClient.getFile(sFilePath);
-    OutStream os = file.getOutStream(WriteType.MUST_CACHE);
-    //
-    for (long k = 0; k < bytes; k ++) {
-      os.write((byte) k);
+  public static void createFiles() throws IOException {
+    for (int i = 0; i < files.length; i ++) {
+      int fileId = sTachyonClient.createFile(files[i]);
+      LOG.info("File " + files[i] + " created with id " + fileId);
     }
-    os.close();
-
-    System.out.println("Write to file " + sFilePath + " " + bytes + " bytes");
   }
 
-  public static void readFile() throws IOException {
+  public static void writeFiles() throws IOException {
+    for (int i = 0; i < files.length; i ++) {
+      TachyonFile file = sTachyonClient.getFile(files[i]);
+      OutStream os = file.getOutStream(WriteType.MUST_CACHE);
+      //
+      for (long k = 0; k < bytes; k ++) {
+        os.write((byte) k);
+      }
+      os.close();
+
+      LOG.info("Write to file " + files[i] + " " + bytes + " bytes");
+    }
+  }
+
+  public static void readFiles() throws IOException {
     try {
-      for (int i = 1; i <= numFiles; i ++) {
+      for (int i = 0; i < files.length; i ++) {
         long time = 0;
         int read = 0;
-        TachyonFile file = sTachyonClient.getFile(sFilePath + "." + i);
-        LOG.info("Going to read file " + sFilePath + "." + i + " size " + file.length());
+        TachyonFile file = sTachyonClient.getFile(files[i]);
+        LOG.info("Going to read file " + files[i] + " size " + file.length());
         time = System.nanoTime();
         InStream is = file.getInStream(ReadType.NO_CACHE);
         if (checkData) {
@@ -97,11 +101,10 @@ public class ReadTest {
           }
         }
         time = System.nanoTime() - time;
-        System.out.println("Finished reading file " + sFilePath + "." + i + " time " + time + " \n");
-        Thread.currentThread().sleep(10000);
+        System.out.println("Finished reading file " + files[i] + " time " + time + " \n");
       }
     } catch (Exception e) {
-      LOG.error("got Exception is test "+e.toString());
+      LOG.error("got Exception is test " + e.toString());
     }
   }
 }
